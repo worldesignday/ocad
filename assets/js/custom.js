5683a6c9adac359fb02b28741ca4962edc546169
worldesing <worldesigday@gmail.com>
2021-01-12 14:27:37 -0500

    upload animatios

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,9 +42,46 @@ header.init();
 
 
 var swiper = new Swiper('.swiper-container', {
-      direction: 'vertical',
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
+  direction: 'vertical',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper('.swiper-container2', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    // when window width is >= 576px
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 768px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+  }
+});
